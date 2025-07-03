78236ded7f8d5709ac9b1d08c597de00654c3228
Patryk Mielcarz <patrykmielcarz@users.noreply.github.com>
2025-07-03 21:40:02 +0200

    Update api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -100,8 +100,8 @@ function swapAparat(patientId, swapData) { // Przyjmuje teraz jeden obiekt z dan
   });
 }
 
-function getSluchawkiOptions(producent) {
-  return fetchAPI(`/sluchawki-options/${producent}`);
+async function getSluchawkiOptions(producent) {
+  return await fetchAPI(`/sluchawki-options/${encodeURIComponent(producent)}`);
 }
 function swapSluchawki(patientId, newSluchawkaId) {
   return fetchAPI(`/patients/${patientId}/swap-sluchawki`, {
